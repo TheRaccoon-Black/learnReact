393f3508239323416c1ef210988d30a295595b5a
muhammad fachrurrozi <ozjibuwat@gmail.com>
2024-11-06 12:15:44 +0700

    done: finish middleware

--- src/index.mjs
+++ src/index.mjs
@@ -9,8 +9,20 @@ const loggingMiddleware = (req, res, next) => {
     next();
 }
 
-app.use(loggingMiddleware);
-
+// app.use(loggingMiddleware);
+const resolveIndexByUserId = (req, res, next) => {
+    const { params: { id } } = req;
+    const parseId = parseInt(id);
+    if (isNaN(parseId)) {
+        return res.sendStatus(400);
+    }  
+    const findUserIndex = testing.findIndex((data) => data.id === parseId);
+    if (findUserIndex === -1) {
+        return res.sendStatus(404);
+    }
+    req.findUserIndex = findUserIndex;
+    next();
+}
 
 
 const PORT = process.env.PORT || 3000;
@@ -34,6 +46,10 @@ app.get('/',(req,res) => {
     res.status(200).send({message:"ini testing"});
 });
 
+// app.use(loggingMiddleware,(req,res,next) => {
+//     console.log("finishing middleware");
+//     next();
+// });
 app.get('/api/test',(req,res) => {
     console.log(req.query);
     const  {query:{ filter,value }} = req;
@@ -44,21 +60,26 @@ app.get('/api/test',(req,res) => {
     // return res.status(200).send(testing);
 });
 
-app.get('/api/test/:id',(req,res)=>{
+app.get('/api/test/:id',resolveIndexByUserId,(req,res)=>{
     // res.status(200).send({message:`ini id ${req.params.id}`});
-    console.log(req.params);
-    const parseId = parseInt(req.params.id);
-    console.log(parseId);
-    if(isNaN(parseId)){
-        res.status(404).send({message:"id not found"});
-    }else{
-        const data = testing.find((data)=>data.id === parseId);
-        if(data){
-            res.status(200).send(data);
-        }else{
-            res.status(404).send({message:"id not found"});
-        }
-    }
+    // console.log(req.params);
+    // const parseId = parseInt(req.params.id);
+    // console.log(parseId);
+    // if(isNaN(parseId)){
+    //     res.status(404).send({message:"id not found"});
+    // }else{
+    //     const data = testing.find((data)=>data.id === parseId);
+    //     if(data){
+    //         res.status(200).send(data);
+    //     }else{
+    //         res.status(404).send({message:"id not found"});
+    //     }
+    // }
+    const {findUserIndex} = req;
+    const findUser = testing[findUserIndex];
+    if(!findUser){
+        return res.sendStatus(404);
+    }return res.status(200).send(findUser);
 })
 
 app.post('/api/test',(req,res)=>{
@@ -70,38 +91,40 @@ app.post('/api/test',(req,res)=>{
     return res.send(newData);
 })
 
-app.put('/api/test/:id',(req,res)=>{
-    const { body, params: { id } } = req;
-    const parseId = parseInt(id);
-    if (isNaN(parseId)) {
-        return res.sendStatus(400);
-    }  
-    const findUserIndex = testing.findIndex((data) => data.id === parseId);
-    if (findUserIndex === -1) {
-        return res.sendStatus(404);
-    }
-    testing[findUserIndex] = {id : parseId, ...body};
-    res.send(testing);
+app.put('/api/test/:id',resolveIndexByUserId,(req,res)=>{
+    // const { body, params: { id } } = req;
+    // const parseId = parseInt(id);
+    // if (isNaN(parseId)) {
+    //     return res.sendStatus(400);
+    // }  
+    // const findUserIndex = testing.findIndex((data) => data.id === parseId);
+    // if (findUserIndex === -1) {
+    //     return res.sendStatus(404);
+    // }
+    const { body, findUserIndex} = req;
+    testing[findUserIndex] = {id: testing[findUserIndex].id, ...body};
+    return res.send(testing);
 }
-)
+);
 
-app.patch('/api/test/:id',(req,res)=>{
-    const  { body, params: { id }} = req;
-    const parseId = parseInt(id);
-    if (isNaN(parseId)) return res.sendStatus(400);
-    const findUserIndex = testing.findIndex((data)=>data.id === parseId);
-    if (findUserIndex === -1) return res.sendStatus(404);
+app.patch('/api/test/:id',resolveIndexByUserId,(req,res)=>{
+    const  { body, findUserIndex} = req;
+    // const  { body, params: { id }} = req;
+    // const parseId = parseInt(id);
+    // if (isNaN(parseId)) return res.sendStatus(400);
+    // const findUserIndex = testing.findIndex((data)=>data.id === parseId);
+    // if (findUserIndex === -1) return res.sendStatus(404);
     testing[findUserIndex] = {...testing[findUserIndex], ...body};
     res.status(200).send(testing);  
 })
 
 
-app.delete('/api/test/:id',(req,res)=>{
-    const { params: { id }} = req;
-    const parseId = parseInt(id);
-    if (isNaN(parseId)) return res.sendStatus(400);
-    const findUserIndex = testing.findIndex((data)=>data.id === parseId);
-    if (findUserIndex === -1) return res.sendStatus(404);
+app.delete('/api/test/:id',resolveIndexByUserId,(req,res)=>{
+    const { findUserIndex} = req;
+    // const parseId = parseInt(id);
+    // if (isNaN(parseId)) return res.sendStatus(400);
+    // const findUserIndex = testing.findIndex((data)=>data.id === parseId);
+    // if (findUserIndex === -1) return res.sendStatus(404);
     testing.splice(findUserIndex,1);
     res.status(200).send(testing); 
 })
